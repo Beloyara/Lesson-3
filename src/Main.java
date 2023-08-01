3a4ca477bafd3de18a787dbb722148c560ab7b9a
Beloyara <beloyara@inbox.ru>
2023-08-01 09:39:46 +0500

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main (String [] args) {
+  int age = 14;
+  if (age >= 18) {
+    System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний");
+  }
+  if (age < 18) {
+    System.out.println("Если возраст человека равен " + age + " , то он не достиг совершеннолетия, нужно немного подождать");
+  }
+}
 }

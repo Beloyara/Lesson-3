efc1d227fb52ca3ee9349a5bf8edc4e2e71d08fa
Beloyara <beloyara@inbox.ru>
2023-08-01 10:55:15 +0500

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ public class Main {
   int age = 14;
   if (age >= 18) {
     System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний");
-  }
-  if (age < 18) {
+  } else {
     System.out.println("Если возраст человека равен " + age + " , то он не достиг совершеннолетия, нужно немного подождать");
   }
-}
-}
+
+  int temperature = 8;
+  if (temperature < 5) {
+System.out.println("На улице " + temperature + " градуса, нужно надеть шапку");
+} else {
+    System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+  }
+}}
